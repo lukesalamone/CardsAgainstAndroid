2eb6f9b4d000f06855896b1870607fcbc9860636
Luke Salamone <lukesalamone94@gmail.com>
2015-10-28 12:56:49 -0500

    Fixed up MainActivity

--- app/src/main/java/io/exis/cards/cards/MainActivity.java
+++ app/src/main/java/io/exis/cards/cards/MainActivity.java
@@ -1,5 +1,7 @@
 package io.exis.cards.cards;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -10,28 +12,32 @@ import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    static final String CONTENT_RATING = "contentRating";
     public boolean adult = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        final Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on click
-            }
-        });
+
+        if (savedInstanceState != null) {
+            adult = savedInstanceState.getBoolean(CONTENT_RATING);
+        }
+
+        this.setPoints();
     }
+
+    public void startGame(View view) {
+        Intent intent = new Intent(view.getContext(), Game.class);
+        view.getContext().startActivity(intent);
+    }
+
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         // The activity is about to be destroyed.
     }
-
-
 
     public void onRadioButtonClicked(View view){
         boolean checked = ((RadioButton) view).isChecked();
@@ -48,5 +54,16 @@ public class MainActivity extends AppCompatActivity {
                 }
         }
     }//end onRadioButtonClicked method
+
+    /*
+     * @TODO
+     * Set points to previously saved value
+     *
+     */
+    private void setPoints(){
+        // 1. Restore from network
+
+        // 2. Display correct number in activity
+    }
 
 }
